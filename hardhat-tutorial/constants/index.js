8f25870589e7e6ea1a8b26496a7e86b5fbd25efb
menotbf <menotyourbestfriend@gmail.com>
2022-09-22 15:42:11 +0600

    change link

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xc3ed7c931f08eda6569ae071193e2e2c8503f73d";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-alsux82si-hhiruko.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-git-master-hhiruko.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
